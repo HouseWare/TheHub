bb675b2f8caf1b0cbda76920940ab9ddfadb1cf8
andyweaver <atweaver@gmail.com>
2014-03-15 15:51:14 -0700

    updating bridge.py to protocols discussed

--- logic/bridge.py
+++ logic/bridge.py
@@ -1,39 +1,39 @@
 #!/usr/bin/env python
 
+
+print_messages = True
+
 import sys
 import time
 import serial
 import queue
 import threading
+import re
 
 
-running = True
 
+class Bridge():
 
-class bridge:
+	
+		def __init__(self, from_hw_queue, device_id=0, sensor_ids=[]):
+			self.device_id = device_id
+			self.sensor_ids = sensor_ids#{"A1".."AZ","D1".."DZ"}, list of sensor IDs as outlined in protocol
+			self.from_hw = from_hw_queue
+			
+			self.to_hw = queue.Queue()
+			self.running = True
+			self.serialport = '/dev/ttyUSB0'
+			self.serialrate = 9600
+			self.myserial = None
+			self.stopped = True
+			self.test = False
+			
+			if test:
+				self.fromhwtest = queue.Queue()
 
-		to_hw = queue.Queue()
-		from_hw = None
-		running = True
-		serialport = '/dev/ttyUSB0'
-		serialrate = 9600
-		myserial = None
-		stopped = True
-		test = False
-
-		#possibly do something with these
-		def serialport(self): return self._serialport
-		def serialport(self, value):self._serialport = value
-
-		def from_hw(self): return self._from_hw
-		def from_hw(self, value):self._from_hw = value
-
-		def __init__(self):
-			pass
-
-		#stop send the kill message to the thread
+		#stop, send the kill message to the thread
 		def stop(self):
-			to_hw.write("kill")
+			self.to_hw.put_nowait("kill")
 
 
 		def start(self):
@@ -42,7 +42,8 @@ class bridge:
 			if ((self.from_hw != None) and (self.serialport != None) and (self.serialrate != None) and self.stopped):
 				if (self.test):#testing
 						mythread = threading.Thread(target=self.theprocess)
-						print("starting process")
+						if print_messages:
+							print("starting process")
 						mythread.start()
 				else:
 						#setup serial and start thread
@@ -52,29 +53,84 @@ class bridge:
 								mythread.start()
 			else:
 				#kick and scream
-				print("kicking and screaming")
+				
+				if (self.from_hw == None):
+					print("from Hardware queue not yet set")
+				if (self.serialport == None):
+					print("Serial Port not set")
+				if (self.serialrate == None):
+					print("Serial Rate not set")
+				if not(self.stopped):
+					print("Already running");
+					
+		
 					
 		def theprocess(self):
 				self.stopped = False
 				self.running = True
 				while self.running:
-					#print ("running=" + str(self.running))
-					
+										
 					#message for hardware
 					if (not(self.to_hw.empty())):
 						msg = str(self.to_hw.get_nowait())
 						if (msg == 'kill'):
 							self.running = False
+							if print_messages:
+								print(" Got a kill command")
 						else:
 							if (not(self.test)):
 								self.myserial.write(msg)
-							print (" [*] Wrote message to hardware: " + msg)
+							if print_messages:
+								print (" [*] Wrote message to hardware: " + msg)
+							
+							
 					if (not(self.test)):#not a test
-						hw_msg = str(self.myserial.readline())
+						hw_msg = str(self.myserial.readline())						
 						if (hw_msg != ""):#got a message from the hardward
-								self.from_hw.put_nowait(hw_msg)
-								print (" [x] Got message from hardware: " + hw_msg)
+								if print_messages:
+									print (" [x] Got message from hardware: " + hw_msg)
+								self.from_hw.put_nowait(self.translate_message_fromhw(hw_msg))
+								if print_messages:
+									print (" [x] Translated message from hardware: " + self.translate_message_fromhw(hw_msg))
+								
+					else:#is a test
+						if (not( self.fromhwtest.empty())):
+							hw_msg = self.fromhwtest.get()			
+							if print_messages:				
+								print (" [x] Got test message from hardware: " + hw_msg)
+							self.from_hw.put_nowait(self.translate_message_fromhw(hw_msg))					
+							
 
 				if (not(self.test)):#not a test, clean up
 						self.myserial.close()
 				self.stopped = True
+				
+				
+		def send_message(self, themessage):
+			#translated_message = translate_message_tohw(themessage)
+			if print_messages:
+				print (" [x] Got message to pass to hardware: " + themessage)
+			
+			if themessage == "kill":
+				to_hw.to_hw.put_nowait("kill")
+			else:
+				#default behavior get all
+				for sensor in self.sensor_ids:
+					if re.match("[A-Z][A-Z0-9]",sensor):
+						self.to_hw.put_nowait(sensor);
+					else:
+						print("Bad sensor ID: "+sensor)
+		
+		
+		#place holding for message translation
+		def translate_message_tohw (themessage):
+			#do some translating eventually
+			outmessage = themessage
+			return outmessage
+
+		def translate_message_fromhw (self,themessage):
+			#do some translating			
+			if themessage[0] == "V":
+				outmessage = [themessage[0],self.device_id,themessage[1:3],int(themessage[3:6])]
+				
+			return outmessage
--- logic/tests/test.py
+++ logic/tests/test.py
@@ -1,18 +1,32 @@
 import queue
 import bridge
-
-bridgetest = bridge.bridge()
-
-bridgetest.test = True
-
 fromhardware = queue.Queue()
 
-bridgetest.from_hw = fromhardware
+#Bridge.Bridge( fromhardware(queue), deviceID, list of sensorIDs)
 
-print("pressing start")
-bridgetest.start()
+bridges = [bridge.Bridge(fromhardware, 0,["A0","A1","D1"]),
+           bridge.Bridge(fromhardware, 1,["A4","A6","D4"]),
+           bridge.Bridge(fromhardware, 2,["A4","A6","D4"])]
 
-print("putting stuff in the queue")
-bridgetest.to_hw.put_nowait("testmsg1")
-bridgetest.to_hw.put_nowait("testmsg2")
-bridgetest.to_hw.put_nowait("kill")
+
+for thisbridge in bridges:
+    print("pressing start on: "+ str(thisbridge.device_id))
+    thisbridge.start()
+
+    #all messages currently send a get all values to device
+    thisbridge.send_message("testmsg1")
+    thisbridge.send_message("testmsg2")
+    for sensor in thisbridge.sensor_ids:
+        thisbridge.fromhwtest.put_nowait("V"+sensor+"999")
+    
+print("checking queue")
+while not fromhardware.empty():
+    themessage = fromhardware.get()
+
+    #for return values from sensor message:
+    #[message commmand(string), deviceID(int), sensor(string), value(int)]
+    print(themessage[0]+str(themessage[1])+themessage[2]+str(themessage[3]))
+
+for thisbridge in bridges:    
+    thisbridge.stop()
+print("stoppped")
