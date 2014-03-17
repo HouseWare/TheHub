70c2861c10626044cf8712740300b233038d9356
kyle-rader <kyle.rader@ieee.org>
2014-03-17 12:34:27 -0700

    Consume device list from API.

--- web/lib/rsrc/js/main.js
+++ web/lib/rsrc/js/main.js
@@ -1,3 +1,15 @@
+var cur_device;
+var cur_sensor;
+
 $(document).ready(function() {
-    alert("Ready!");
+    get_devices();
 });
+
+function get_devices() {
+    var list = $('.device-list');
+    $.get("/api/device/getdevices", function(data) {
+	data['devices'].forEach(function(device) {
+	    list.append("<li><a href=\"\" data-id=" + device['id'] + ">" + device['description'] + "</a></li>");	    
+	});
+    });
+};
