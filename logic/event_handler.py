ae22a10abcad7777127f691503c60450e6091716
Jeff <jkuan@live.com>
2014-04-22 19:05:39 -0700

    Applied PEP8

--- logic/event_handler.py
+++ logic/event_handler.py
@@ -1,66 +1,86 @@
 #!/usr/bin/env python3
 
-# HouseWare
-# Jeffrey Kuan
-# 3/15/14
+"""
+HouseWare
+Jeffrey Kuan
+4/22/14
+"""
 
-# Packages/modules
-from package import Package
-##from sqlalchemy import *
+# Imports
 import time
 import threading
 import queue
 import bridge
+from ..database import db
 
 # Event handler class
-# A logic unit that communicates with devices and other units of the hub including the web server and database by processing messages as necessary
-class Event_Handler(inbox, devices):
-	running = 1;
+# A logic unit class that communicates with devices, the web server, and the database by applying logic to messages as necessary
+class EventHandler(inbox, devices):
+    running = True;
+
 
 # Class constructor
-# Parameter(s): a queue of messages and an array of devices
+# Parameter(s): a queue for messages and an array of devices
 def __init__(self, inbox, devices)
-	self.inbox = inbox
-	self.devices = devices
+    self.inbox = inbox
+    self.devices = devices
 
-# Main run method
-# Parameter(s): n/a
-def run(self)
-	# Event loop
-	while running:
-		# Time delay
-		start = time.clock()
-		##Call refresh device method
-		
-		# Queue check
-		while (time.clock() < (start + 60)):
-			__pop()
+    # Main run method
+    # Parameter(s): n/a
+    def run(self)
 
-# Queue push method
-# Parameter(s): a message to be pushed onto the queue
-def push(self, message)
-	inbox.put(message)
+        # Main event loop
+        while running:
+            start = time.clock()    # Starts time delay
+            refresh()
 
-# Queue pop method
-# Parameter(s): n/a
-def __pop(self)
-	# Empty queue
-	if inbox.empty():
-		pass
-	else:
-		message = inbox.get()
-		# Data update received
-		if :
-			##Write to database
-		# Refresh request received
-		elif :
-			##Call refresh device method
-		# Termination signal received
-		else:
-			running = 0
+            # Check queue for messages
+            while time.clock() < (start + 60):
+                pop()
 
-# Class destructor
-# Parameter(s): n/a
-def __del__(self):
-	# Send confirmation message to main control queue
-	pass
+    # Queue push method
+    # Parameter(s): a message to be pushed onto the queue
+    def push(self, message)
+        inbox.put(message)
+
+    # Queue pop method
+    # Parameter(s): n/a
+    def pop(self)
+
+        # No messages
+        if inbox.empty():
+            pass
+
+        # Messages found
+        else:
+            message = inbox.get()
+
+            # Data update received
+            if """""":
+               write(message)
+
+            # Refresh request received
+            elif """""":
+                refresh()
+
+            # Termination signal received
+            else:   
+                running = False
+
+    # Request refreshed sensor values
+    # Parameter(s): n/a
+    def refresh(self)
+
+        # Send message to each device
+        for x in devices:
+            x.send_message("refresh")
+
+    # Write sensor values to database and apply logic if necessary
+    # Parameter(s): a database statement
+    def write(self, message)
+        """""""
+
+    # Class destructor
+    # Parameter(s): n/a
+    def __del__(self)
+        pass
