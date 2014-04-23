b70b7b9b5326b4e42eb6c840a5ee31723a55600d
Reilly Tucker Siemens <reilly.siemens@gmail.com>
2014-04-22 18:09:42 -0700

    Scaffold multiple thread support in TheHub/__main__.py.

--- __main__.py
+++ __main__.py
@@ -1,10 +1,28 @@
 #TODO: Add another thread for the event handler.
 
+import queue
 import threading
 from TheHub.web import app as web_app
+from TheHub.logic import event_handler as event_loop
 
-print("TheHub is running.")
-print("Starting web app...")
+# Create an empty list in which to place our threads.
+threads = []
 
-web_thread = threading.Thread(target = web_app)
-web_thread.start()
+event_queue = queue.Queue()
+
+print("Creating threads.")
+web_thread = threading.Thread(
+        name = "Web App",
+        target = web_app
+)
+event_thread = threading.Thread(
+        name = "Event Loop",
+        target = event_loop
+)
+
+threads.extend([web_thread, event_thread])
+
+print("Starting threads.")
+for thread in threads:
+    thread.start()
+    print("Started thread: {}".format(thread.name))
