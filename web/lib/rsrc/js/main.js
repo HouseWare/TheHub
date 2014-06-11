00c768f56a38a3b5fd1e6d157e071b0c284412df
kyle-rader <kyle.rader@ieee.org>
2014-06-10 19:07:37 -0700

    Move graph code into get_sensor_data callback and add CSS

--- web/lib/rsrc/js/main.js
+++ web/lib/rsrc/js/main.js
@@ -47,13 +47,45 @@ function get_sensors(device_id) {
 };
 
 function get_sensor_data(sensor_id) {
-    var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-    var data;
-    console.log('Getting sensor ' + sensor_id + ' data:');
+
     $.get('/api/sensor/' + sensor_id + '/getevents/2014-01-01+00:00:00', function(response) {
-	data = response.data_events;
-	console.log(data);
+	var data = response.data_events;
+	var margin = {top: 20, right: 30, bottom: 30, left: 40},
+	width = 960 - margin.left - margin.right,
+	height = 500 - margin.top - margin.bottom;
+
+	var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
+	var y = d3.scale.linear().range([height, 0]);
+	var xAxis = d3.svg.axis().scale(x).orient("bottom");
+	var yAxis = d3.svg.axis().scale(y).orient("left");
+
+	d3.select(".chart").selectAll("*").remove();
+	var chart = d3.select(".chart")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+	    .append("g")
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	x.domain(data.map(function(d) { return d.timestamp; }));
+	y.domain([0, d3.max(data, function(d) { return d.value; })]);
+	
+	chart.append("g")
+	    .attr("class", "x axis")
+	    .attr("transform", "rotate(-90)")
+	    .attr("transform", "translate(0," + height + ")")
+	    .call(xAxis);
+
+	chart.append("g")
+	    .attr("class", "y axis")
+	    .call(yAxis);
+
+	chart.selectAll(".bar")
+	    .data(data)
+	    .enter().append("rect")
+	    .attr("class", "bar")
+	    .attr("x", function(d) { return x(d.timestamp); })
+	    .attr("y", function(d) { return y(d.value); })
+	    .attr("height", function(d) { return height - y(d.value); })
+	    .attr("width", x.rangeBand());
     });
 };
