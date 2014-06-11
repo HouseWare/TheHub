cd117cb235dcd50e9798460f8e47dbf9b8c442cd
kyle-rader <kyle.rader@ieee.org>
2014-06-10 17:13:30 -0700

    Setup new sensor data callbacks

--- web/lib/rsrc/js/main.js
+++ web/lib/rsrc/js/main.js
@@ -2,15 +2,14 @@ var cur_device;
 var cur_sensor;
 
 $(document).ready(function() {
-    console.log($.fn.jquery);
     get_devices();
     $(document).foundation();
 });
 
-$(document).on('click', '.device-btn', function(btnClick) {
+$(document).on('click', '.sensor-btn', function(btnClick) {
     var device_id = btnClick.target.attributes['data-id'].value;
-    get_sensors(device_id);
-    $(document).foundation();
+    get_sensor_data(device_id);
+    //$(document).foundation();
 });
 
 function get_devices() {
@@ -21,6 +20,7 @@ function get_devices() {
 	    list.append("<li><a class=\"device-btn\" data-id=" + device['id'] + ">" + device['description'] + "</a></li>");
 	    if (first) {
 		get_sensors(device['id']);
+		cur_device = device['id'];
 		first = false;
 	    };
 	});
@@ -36,9 +36,13 @@ function get_sensors(device_id) {
 
     $.get('/api/device/' + device_id + '/getsensors', function(data) {
 	data['sensors'].forEach(function(sensor) {
-	    btnsGrp.append('<li ' + (first ? active : '>') + '<a class="button button-small" href="#sen' + sensor['id'] + '">' + sensor['description'] + '</a></li>');
+	    btnsGrp.append('<li ' + (first ? active : '>') + '<a class="button sensor-btn" data-id=' + sensor['id'] + '>' + sensor['description'] + '</a></li>');
+	    if(first) {
+		cur_sensor = sensor['id'];
+	    }
 	    first = false;
 	});
+	get_sensor_data(cur_sensor);
     });
 };
 
@@ -46,13 +50,10 @@ function get_sensor_data(sensor_id) {
     var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
-
-    
-    $.ajax({
-	url:'/api/sensor/' + sensor_id + '/getevents/2014-01-01+00:00:00',
-	type: 'GET',
-	async: false,
-	success: function(data) {
-	    output = data['data_events'];
-	}});
+    var data;
+    console.log('Getting sensor ' + sensor_id + ' data:');
+    $.get('/api/sensor/' + sensor_id + '/getevents/2014-01-01+00:00:00', function(response) {
+	data = response.data_events;
+	console.log(data);
+    });
 };
